1ddb45d326a34818bfc4e0a02a3c7a6a5094397c
Isaiah Gordon <GordIsai857@GoTVDSB.ca>
2022-11-17 09:40:07 -0500

    add timer

--- Game1.cs
+++ Game1.cs
@@ -1,18 +1,25 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Timers;
 
 namespace textTitleColor
 {
     public class Game1 : Game
-    {
+    {        
+        SpriteEffects effect;
+        Timer timer = new Timer(2000);
         Rectangle maxRect = new Rectangle(0, 0, 800, 500),minRect=new Rectangle(0,0,100,100);
         Texture2D dinotexture,seagul2d;
         Point mousePoint;
         SpriteFont sprite;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private static void TimerElapsed(Object source)
+        {
 
+        }
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -43,29 +50,27 @@ namespace textTitleColor
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            else if (Keyboard.GetState().IsKeyDown(Keys.A))
-            {                
-                mousePoint = Mouse.GetState().Position;
-                _spriteBatch.Begin();
-                _spriteBatch.DrawString(sprite,mousePoint.ToString(),new Vector2(400,0),Color.Blue);
-                _spriteBatch.End();
-            }
-
             // TODO: Add your update logic here
-
+            var kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.A))
+                mousePoint = Mouse.GetState().Position;
+            else
+                timer.Start();
             base.Update(gameTime);
         }
-
+        
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            _spriteBatch.Draw(seagul2d,maxRect, Color.White);
-            _spriteBatch.Draw(dinotexture, minRect, Color.Red);
+            _spriteBatch.Draw(seagul2d,maxRect,Color.White);
+            _spriteBatch.Draw(dinotexture, minRect,minRect, Color.Red,new float(),new Vector2(0,0),effect,new float()); 
+            _spriteBatch.DrawString(sprite, mousePoint.ToString(), new Vector2(400, 0), Color.Blue);
             _spriteBatch.End();
             base.Draw(gameTime);
         }
+        
     }
 }
