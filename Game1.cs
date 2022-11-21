c331ab780a359eacfe84115a81116823e3256619
Isaiah Gordon <GordIsai857@GoTVDSB.ca>
2022-11-21 09:39:39 -0500

    blub blub

--- Game1.cs
+++ Game1.cs
@@ -7,21 +7,17 @@ using System.Timers;
 namespace textTitleColor
 {
     public class Game1 : Game
-    {        
-        SpriteEffects effect;
-        Timer timer = new Timer(2000);
-        Rectangle maxRect = new Rectangle(0, 0, 800, 500),minRect=new Rectangle(0,0,100,100);
-        Texture2D dinotexture,seagul2d;
+    {
+        Vector2 dinospeed = new Vector2(2, 2),speedonib= new Vector2(-2,-2);
+        Rectangle maxRect = new Rectangle(0, 0, 800, 500), minRect = new Rectangle(0, 0, 100, 100), tceRnim = new Rectangle(700, 0, 100, 100), size = new Rectangle();
+        Texture2D dinotexture,seagul2d,textureonib;
         Point mousePoint;
         SpriteFont sprite;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        private static void TimerElapsed(Object source)
-        {
-
-        }
+        
         public Game1()
-        {
+        {            
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
@@ -44,6 +40,7 @@ namespace textTitleColor
             dinotexture=Content.Load<Texture2D>("dino (2)");
             seagul2d = Content.Load<Texture2D>("seaguls");
             sprite = Content.Load<SpriteFont>("Arial");
+            textureonib = Content.Load<Texture2D>("onib");
         }
 
         protected override void Update(GameTime gameTime)
@@ -51,11 +48,21 @@ namespace textTitleColor
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             // TODO: Add your update logic here
+
             var kstate = Keyboard.GetState();
             if (kstate.IsKeyDown(Keys.A))
                 mousePoint = Mouse.GetState().Position;
-            else
-                timer.Start();
+            else if (kstate.IsKeyDown(Keys.D))
+            {
+                minRect.Y = mousePoint.Y;
+                minRect.X = mousePoint.X;
+                tceRnim.Y = mousePoint.Y;
+                tceRnim.X = mousePoint.X+100;
+            }
+            
+            
+                
+            
             base.Update(gameTime);
         }
         
@@ -66,8 +73,9 @@ namespace textTitleColor
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
             _spriteBatch.Draw(seagul2d,maxRect,Color.White);
-            _spriteBatch.Draw(dinotexture, minRect,minRect, Color.Red,new float(),new Vector2(0,0),effect,new float()); 
+            _spriteBatch.Draw(dinotexture, minRect, Color.White); 
             _spriteBatch.DrawString(sprite, mousePoint.ToString(), new Vector2(400, 0), Color.Blue);
+            _spriteBatch.Draw(textureonib, tceRnim, Color.White);
             _spriteBatch.End();
             base.Draw(gameTime);
         }
